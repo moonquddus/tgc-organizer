f189a4371615583c81a0b8e301111461c2867e5a
Moon Quddus <moonquddus@gmail.com>
2019-01-03 22:33:23 +0000

    Beginnings of integrating cards into grid

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -15,27 +15,40 @@ class Editor extends Component {
     createAlbum() {
         let album = [];
         for (let i = 0; i < this.pageCount; i++) {
-            album.push(<h2>{`Page ${(i + 1)}`}</h2>);
-            album.push(<div className="grid" key={i}>{this.createGrid()}</div>)
+            album.push(<h2 key={"h2-" + i}>{`Page ${(i + 1)}`}</h2>);
+            album.push(<div className="grid" key={i}>{this.createGrid(i + 1)}</div>)
         }
-        console.log("ALBUM", album);
         return album;
     }
 
-    createGrid = () => {
+    createGrid(page) {
         let grid = []
-    
+        let state = store.getState();
+
         // Outer loop to create rows
         for (let i = 0; i < this.height; i++) {
           let children = []
           //Inner loop to create columns
           for (let j = 0; j < this.width; j++) {
-            children.push(<div className="grid-column" key={j}>{`Card ${(i * 3) + (j + 1)}`}</div>)
+            let cardNo = ((page-1)*state.grid.x*state.grid.y) + ((i * state.grid.x) + (j + 1));
+            
+            let cardInner = [`CARD ${cardNo}`];
+            if (state.cards[cardNo - 1]) {
+                cardInner.push(<img key={"img-" + cardNo} className="cell-image" src={state.cards[cardNo - 1].img} alt={state.cards[cardNo - 1].name} />);
+            }
+
+            children.push(<div className="grid-cell" key={page+"-"+i+"-"+j} onClick={this.handleCellClick.bind(cardNo)}>{cardInner}</div>)
           }
           //Create the row and add the columns
-          grid.push(<div className="grid-row" key={i}>{children}</div>)
+          grid.push(<div className="grid-row" key={page+"-"+i}>{children}</div>)
         }
         return grid
+    }
+
+    handleCellClick() {
+        let state = store.getState();
+        let cardNo = this;
+        console.log("CARD:", state.cards[cardNo - 1]);
     }
 
 
